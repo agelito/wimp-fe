316503fe71afd0ac8a77394a2c7ee4e381d5eec4
Axel Wettervik <axel.wettervik@gmail.com>
2021-10-14 00:35:47 +0900

    fix: store dates as strings in redux (#6)
    * store dates as strings in redux
    * add reducer to remove old intel
    * change intel sorting order

--- src/Dtos/Wimp/ReadPictureDto.ts
+++ src/Dtos/Wimp/ReadPictureDto.ts
@@ -1,8 +1,8 @@
 import { ReadIntelDto } from "./ReadIntelDto";
 
 export type ReadPictureDto = {
-    since_time: Date,
-    generated_time: Date,
+    since_time: string,
+    generated_time: string,
     reported_intel: ReadIntelDto[],
     reported_characters: number[],
     reported_ships: number[],
