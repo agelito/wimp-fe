d89bd52108015c43be956ba50fdc97642891868c
Axel Wettervik <axel.wettervik@gmail.com>
2021-10-17 23:52:01 +0900

    feat: add support for clear and intel age coloring

--- src/Components/UniverseMap/MapNode.tsx
+++ src/Components/UniverseMap/MapNode.tsx
@@ -8,6 +8,8 @@ import { ReportedIntelLabel } from './ReportedIntelLabel/ReportedIntelLabel';
 import { SystemData } from './UniverseMap';
 import "./MapNode.css";
 import { selectIntelForSystem, selectPicture } from '../../State/Picture/pictureSlice';
+import { interpolateRgbBasis } from "d3-interpolate";
+import { getCurrentDateUTC } from '../../Utils/DateUtils';
 
 interface Props {
     data: SystemData
@@ -27,7 +29,20 @@ const MapNode: React.FC<Props> = ({ data, style }) => {
     const intelInSystem = useMemo(() => {
         if (!intelPicture) return [];
         return selectIntelForSystem(intelPicture, data.systemId)
+            .sort((a, b) => Date.parse(b.timestamp) - Date.parse(a.timestamp));
     }, [data.systemId, intelPicture]);
+
+    const intelInSystemSinceClear = useMemo(() => {
+        const mostRecentClearIntel =
+            intelInSystem.filter(i => i.isClear)[0];
+
+        if (mostRecentClearIntel) {
+            const clearTime = Date.parse(mostRecentClearIntel.timestamp);
+            return intelInSystem.filter(i => Date.parse(i.timestamp) > clearTime);
+        } else {
+            return intelInSystem;
+        }
+    }, [intelInSystem])
 
     const nodeBackgroundColor = useMemo(() => {
         if (isSelected) {
@@ -38,6 +53,26 @@ const MapNode: React.FC<Props> = ({ data, style }) => {
             return theme.palette.neutralQuaternary;
         }
     }, [isLocatedAt, isSelected, theme.palette.neutralQuaternary, theme.palette.neutralQuaternaryAlt, theme.palette.neutralTertiaryAlt]);
+
+    const warningColorInterpolation = useMemo(() => {
+        return interpolateRgbBasis([theme.palette.green, theme.palette.yellow, theme.palette.red]);
+    }, [theme]);
+
+    const mostRecentIntelSinceClear = useMemo(() => intelInSystemSinceClear[0], [intelInSystemSinceClear]);
+
+    const warningColor = useMemo(() => {
+        if (mostRecentIntelSinceClear === undefined) {
+            return warningColorInterpolation(0.0);
+        }
+
+        const currentEveTime = getCurrentDateUTC().getTime();
+        const mostRecentTimestamp = Date.parse(mostRecentIntelSinceClear.timestamp);
+        const threatFadeAwayInSeconds = 15 * 60; // 15 minutes
+
+        const secondsSinceRecentInSystem = (currentEveTime - mostRecentTimestamp) / 1000.0;
+
+        return warningColorInterpolation(1.0 - secondsSinceRecentInSystem / threatFadeAwayInSeconds);
+    }, [mostRecentIntelSinceClear, warningColorInterpolation]);
 
     return (
         <>
@@ -60,7 +95,7 @@ const MapNode: React.FC<Props> = ({ data, style }) => {
                     width: "100%",
                     height: "100%",
                     borderRadius: "50%",
-                    backgroundColor: intelInSystem.length > 0 ? theme.palette.red : theme.palette.green,
+                    backgroundColor: warningColor,
                     boxShadow: DefaultEffects.elevation16,
                 }}>
                     <div style={{
@@ -70,7 +105,7 @@ const MapNode: React.FC<Props> = ({ data, style }) => {
                         backgroundColor: nodeBackgroundColor,
                         ...style
                     }}>
-                        <ReportedIntelLabel reportedIntelCount={intelInSystem.length} />
+                        <ReportedIntelLabel reportedIntelCount={intelInSystemSinceClear.length} />
                     </div>
                 </div>
             </div>
--- src/State/Picture/pictureSlice.ts
+++ src/State/Picture/pictureSlice.ts
@@ -3,14 +3,17 @@ import { RootState } from "../store";
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { ReadPictureDto } from "../../Dtos/Wimp/ReadPictureDto";
 import { ReadIntelDto } from "../../Dtos/Wimp/ReadIntelDto";
+import { getCurrentDateUTC } from "../../Utils/DateUtils";
 
 interface PictureState {
     generatedDate?: string,
     characterIntel: ReadIntelDto[];
+    statusIntel: ReadIntelDto[];
 };
 
 const initialState: PictureState = {
     characterIntel: [],
+    statusIntel: [],
 };
 
 export const pictureSlice = createSlice({
@@ -22,15 +25,7 @@ export const pictureSlice = createSlice({
             const sortedIntel = [...picture.reported_intel]
                 .sort((a, b) => Date.parse(a.timestamp) - Date.parse(b.timestamp));
 
-            const now = new Date();
-            var currentUtcMillis = new Date(
-                now.getUTCFullYear(),
-                now.getUTCMonth(),
-                now.getUTCDate(),
-                now.getUTCHours(),
-                now.getUTCMinutes(),
-                now.getUTCSeconds()
-            ).valueOf();
+            var currentUtcMillis = getCurrentDateUTC().valueOf();
 
             // NOTE: 15 = 15 minutes, 60000 = 1 minute in ms
             var removeAfterUtcMillis = (currentUtcMillis - 15 * 60000);
@@ -45,20 +40,22 @@ export const pictureSlice = createSlice({
                     characters.set(i.character.id, i);
                 });
 
+            var withoutCharacters = new Map(state.statusIntel
+                .filter(ci => Date.parse(ci.timestamp) > removeAfterUtcMillis)
+                .map(ci => [ci.id, ci]));
+
+            sortedIntel
+                .filter(i => i.character === null)
+                .forEach(i => {
+                    withoutCharacters.set(i.id, i);
+                });
+
             state.characterIntel = Array.from(characters.values());
+            state.statusIntel = Array.from(withoutCharacters.values());
             state.generatedDate = picture.generated_time;
         },
         removeReportsOlderThanMinutes: (state, action: PayloadAction<number>) => {
-            const now = new Date();
-            var currentUtcMillis = new Date(
-                now.getUTCFullYear(),
-                now.getUTCMonth(),
-                now.getUTCDate(),
-                now.getUTCHours(),
-                now.getUTCMinutes(),
-                now.getUTCSeconds()
-            ).valueOf();
-
+            var currentUtcMillis = getCurrentDateUTC().valueOf();
             var removeAfterUtcMillis = (currentUtcMillis - action.payload * 60000);
 
             var characterIntel = state.characterIntel.filter(ci =>
@@ -87,5 +84,7 @@ export const { useGetPictureQuery } = wimpPictureApi
 export const { addPicture, removeReportsOlderThanMinutes } = pictureSlice.actions;
 export const selectPicture = (state: RootState) => state.picture;
 export const selectIntelForSystem = (state: PictureState, systemId: number) =>
-    state.characterIntel.filter(intel => intel.starSystem.id === systemId);
+    state.characterIntel
+        .concat(state.statusIntel)
+        .filter(intel => intel.starSystem.id === systemId);
 export default pictureSlice.reducer;
